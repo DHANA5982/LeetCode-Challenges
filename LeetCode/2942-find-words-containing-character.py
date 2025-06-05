e3f6cde26ab93c482c11fc7922570cc81e09ac65
DHANA5982 <149905097+DHANA5982@users.noreply.github.com>
2025-06-05 15:31:40 +0100

    UPDATED LC challenge 2942

--- LeetCode/2942-find-words-containing-character.py
+++ LeetCode/2942-find-words-containing-character.py
@@ -1,14 +1,5 @@
-# Basic definition function
-def find_words_contain_char(words, letter):
-    return [i for i, word in enumerate(words) if letter in word]
-
-if __name__== '__main__':
-    words = list(input('Enter words seperated by space: ').split())
-    x = input('Enter letter you want to find: ')
-    print(find_words_contain_char(words, x))
-
-# Class method
 from typing import List
+
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         return [index for index, word in enumerate(words) if x in word]
@@ -16,6 +7,5 @@ class Solution:
 if __name__ == '__main__':
     word = input('Enter words seperated by space: ').split()
     search_term = input('Enter letter you want to find: ')
-    
     solver = Solution()
-    result = solver.findWordsContaining(word, search_term)
+    print(solver.findWordsContaining(word, search_term))
