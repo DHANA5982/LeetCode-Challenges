8f97de44aa691d0484e2a03c49e395e7a9602704
DHANA5982 <sekar.dhana5982@gmail.com>
2025-10-27 18:05:49 +0000

    SOLVE Q13

--- SQLPractice/013.KingPlayersPerCountry.sql
+++ SQLPractice/013.KingPlayersPerCountry.sql
@@ -1,8 +1,6 @@
 -- Active: 1761408174101@@localhost@5432@PostgreSQLDB
 USE PostgreSQLDB;
 
-SELECT NAME FROM sys.tables;
-
 CREATE TABLE King_Players_Demographic (
     player_id INTEGER PRIMARY KEY,
     registration_date DATE,
@@ -70,60 +68,3 @@ INSERT INTO King_Level_Info VALUES
 SELECT COALESCE(country, 'Unknown') AS country, COUNT(*) AS total_players
 FROM King_Players_Demographic
 GROUP BY COALESCE(country, 'Unknown');
-
-
---- Find the top 5 countries with the highest average minutes played per player.
---- in the last 30 days (2025-01-03 to 2025-02-01).
-
-SELECT COALESCE(d.country, 'Unknown') AS country, ROUND(AVG(avg_minutes_per_player),2) AS player_avg_minutes
-FROM (
-    SELECT player_id, AVG(minutes_played) avg_minutes_per_player
-    FROM King_Player_Activity
-    WHERE activity_date >= '2025-01-03' AND activity_date < '2025-02-01'
-    GROUP BY player_id
-) AS a
-JOIN King_Players_Demographic AS d
-ON d.player_id = a.player_id
-GROUP BY COALESCE(d.country, 'Unknown')
-ORDER BY player_avg_minutes DESC
-LIMIT 5;
-
-
---- Find the top 5 hardest levels (lowest success rate) across all players. 
---- considering first-time attempts only, and include their difficulty from level_info.
-
-SELECT l.level_id, li.difficulty,
-        ROUND(AVG(CASE WHEN l.success = 'TRUE' THEN 1.0 ELSE 0.0 END), 2) AS success_rate,
-        count(*) AS total_attempts
-FROM King_Level_Attempt AS l
-JOIN King_Level_Info AS li
-ON l.level_id = li.level_id
-GROUP BY l.level_id, li.difficulty
-ORDER BY success_rate ASC
-LIMIT 5;
-
-WITH first_success AS (
-    SELECT player_id, level_id,
-    MIN(attempt_id) AS first_success_attempt
-    FROM King_Level_Attempt
-    WHERE success = 'TRUE'
-    GROUP BY player_id, level_id
-),
-first_time_attempt AS (
-    SELECT l.*
-    FROM King_Level_Attempt AS l
-    LEFT JOIN first_success AS fs
-    ON l.player_id = fs.player_id
-    AND l.level_id = fs.level_id
-    WHERE fs.first_success_attempt IS NULL
-    OR l.attempt_id <= fs.first_success_attempt
-)
-SELECT fta.level_id, li.difficulty,
-    ROUND(AVG(CASE WHEN fta.success = 'TRUE' THEN 1.0 ELSE 0.0 END), 2) AS success_rate,
-    COUNT(*) AS total_attempts
-FROM first_time_attempt fta
-JOIN King_Level_Info li 
-ON fta.level_id = li.level_id
-GROUP BY fta.level_id, li.difficulty
-ORDER BY success_rate ASC
-LIMIT 5;
